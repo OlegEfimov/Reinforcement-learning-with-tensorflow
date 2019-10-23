d20d91b48a4d557041795e5b0017c6dc7c02839b
ruoefimov <Oleg.Efimov@harman.com>
2019-10-23 21:10:32 +0300

    tmp1

--- experiments/car_example2/car_example2.py
+++ experiments/car_example2/car_example2.py
@@ -1,39 +1,22 @@
-"""
-Policy Gradient, Reinforcement Learning.
-
-The cart pole example
-
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-
-Using:
-Tensorflow: 1.0
-gym: 0.8.0
-"""
-
-import gym
+from car_env import CarEnv
 from RL_brain import PolicyGradient
 import matplotlib.pyplot as plt
 
 DISPLAY_REWARD_THRESHOLD = -2000  # renders environment if total episode reward is greater then this threshold
-# episode: 154   reward: -10667
-# episode: 387   reward: -2009
-# episode: 489   reward: -1006
-# episode: 628   reward: -502
 
-RENDER = True  # rendering wastes time
+DISCRETE_ACTION = False
+RENDER = True
 
-env = gym.make('MountainCar-v0')
-env.seed(1)     # reproducible, general Policy gradient has high variance
-env = env.unwrapped
+env = CarEnv(discrete_action=DISCRETE_ACTION)
+STATE_DIM = env.state_dim
+ACTION_DIM = env.action_dim
+ACTION_BOUND = env.action_bound
 
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
+
 
 RL = PolicyGradient(
-    n_actions=env.action_space.n,
-    n_features=env.observation_space.shape[0],
+    n_actions=STATE_DIM,
+    n_features=ACTION_DIM,
     learning_rate=0.02,
     reward_decay=0.995,
     # output_graph=True,
