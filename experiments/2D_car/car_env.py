d83ee471c80a7956cff1e2aec02b413f5409736e
ruoefimov <Oleg.Efimov@harman.com>
2019-11-01 20:42:50 +0300

    1-size action

--- experiments/2D_car/car_env.py
+++ experiments/2D_car/car_env.py
@@ -18,7 +18,7 @@ pyglet.clock.set_fps_limit(10000)
 
 class CarEnv(object):
     n_sensor = 33
-    action_dim = 2
+    action_dim = 1
     state_dim = n_sensor
     viewer = None
     viewer_xy = (500, 500)
